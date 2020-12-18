48f41bf2a406982693aa2c7dd199882b8d995ad6
Aldona <aldonapawlak2@gamil.com>
2020-12-18 13:26:43 +0100

    Initial commit

---
+++ src/test/java/com/alwo/AlwoOnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlwoOnlineShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
