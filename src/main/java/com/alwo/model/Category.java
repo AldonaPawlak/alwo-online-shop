61e9215e4e3f564d9e7914d1f038a27a8e6d856d
Aldona <aldonapawlak2@gamil.com>
2020-12-18 22:30:01 +0100

    Add Category model

--- src/main/java/com/alwo/model/Category.java
+++ src/main/java/com/alwo/model/Category.java
@@ -2,8 +2,12 @@ package com.alwo.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Setter
 @Getter
