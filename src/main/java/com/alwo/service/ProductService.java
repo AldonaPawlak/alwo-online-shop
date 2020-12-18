9592d4c794ceb650194662cd52e0701c53f6032d
Aldona <aldonapawlak2@gamil.com>
2020-12-18 15:26:10 +0100

    Handle getting products

---
+++ src/main/java/com/alwo/service/ProductService.java
@@ -0,0 +1,23 @@
+package com.alwo.service;
+
+import com.alwo.model.Product;
+import com.alwo.repository.ProductRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private ProductRepository productRepository;
+    private static final int PAGE_SIZE = 10;
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getProducts(int page, Sort.Direction sort) {
+        return productRepository.findAllProducts(PageRequest.of(page, PAGE_SIZE, Sort.by(sort, "name")));
+    }
+}
