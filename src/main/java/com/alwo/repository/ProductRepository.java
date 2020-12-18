dca790bc39ce5638179fb1badd326648b787c46e
Aldona <aldonapawlak2@gamil.com>
2020-12-18 15:25:30 +0100

    Handle product repository

---
+++ src/main/java/com/alwo/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.alwo.repository;
+
+import com.alwo.model.Product;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("select p from Product p")
+    List<Product> findAllProducts(Pageable page);
+}
