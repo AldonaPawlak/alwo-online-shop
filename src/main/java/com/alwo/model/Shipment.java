1943f976e4149d0c392ae14ce94cb70e0c1f4120
wtokarz80 <w.tokarz80gmail.com>
2020-12-18 19:40:52 +0100

    Add shipment and payment entities

---
+++ src/main/java/com/alwo/model/Shipment.java
@@ -0,0 +1,31 @@
+package com.alwo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "shipments")
+public class Shipment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private ShipmentStatus shipmentStatus;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private ShipmentMethod shipmentMethod;
+
+    public Shipment(ShipmentStatus shipmentStatus, ShipmentMethod shipmentMethod) {
+        this.shipmentStatus = shipmentStatus;
+        this.shipmentMethod = shipmentMethod;
+    }
+
+    public Shipment() {
+    }
+}
