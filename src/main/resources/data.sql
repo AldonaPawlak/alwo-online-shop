00bd2efe3bb9616e29016fe1897a945d39a23b54
Aldona <aldonapawlak2@gamil.com>
2021-01-08 09:17:22 +0100

    Update adding dates to db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,8 @@
-INSERT INTO user_roles(role) values ('CUSTOMER');
-INSERT INTO user_roles(role) values ('ADMIN');
-
-INSERT INTO users(email, password, role_id) values ('admin@admin.pl', 'admin123', 2);
-INSERT INTO users(email, password, role_id) values ('ala@ala.pl', 'ala123', 1);
-INSERT INTO users(email, password, role_id) values ('jurek@jurek.pl', 'jurek123', 1);
-INSERT INTO users(email, password, role_id) values ('ula@ula.pl', 'ula123', 1);
-INSERT INTO users(email, password, role_id) values ('zenek@zenek.pl', 'zenek123', 1);
+INSERT INTO users(username, password, user_role) values ('admin@admin.pl', 'admin123', 'ADMIN');
+INSERT INTO users(username, password, user_role) values ('ala@ala.pl', 'ala123', 'CUSTOMER');
+INSERT INTO users(username, password, user_role) values ('jurek@jurek.pl', 'jurek123', 'CUSTOMER');
+INSERT INTO users(username, password, user_role) values ('ula@ula.pl', 'ula123', 'CUSTOMER');
+INSERT INTO users(username, password, user_role) values ('zenek@zenek.pl', 'zenek123', 'CUSTOMER');
 
 INSERT INTO product_types(product_type) values ('PRODUCT_TYPE_1');
 INSERT INTO product_types(product_type) values ('PRODUCT_TYPE_2');
@@ -51,3 +48,27 @@ INSERT INTO basket_products(quantity, total_price, product_id, user_id) values (
 INSERT INTO basket_products(quantity, total_price, product_id, user_id) values (2, 400, 3, 1);
 INSERT INTO basket_products(quantity, total_price, product_id, user_id) values (1, 120, 1, 2);
 INSERT INTO basket_products(quantity, total_price, product_id, user_id) values (5, 2750, 4, 3);
+
+INSERT INTO payment_statuses(payment_status) values ('WAITING');
+INSERT INTO payment_statuses(payment_status) values ('PROCESSED');
+INSERT INTO payment_statuses(payment_status) values ('FAILED');
+INSERT INTO payment_methods(payment_method) values ('CARD');
+
+INSERT INTO shipment_statuses(shipment_status) values ('INITIAL');
+INSERT INTO shipment_statuses(shipment_status) values ('PACKED');
+INSERT INTO shipment_statuses(shipment_status) values ('PICKEDUP');
+INSERT INTO shipment_statuses(shipment_status) values ('SHIPPED');
+INSERT INTO shipment_statuses(shipment_status) values ('DELIVERED');
+
+INSERT INTO shipment_methods(shipment_method, shipment_cost) values ('INPOST', 10);
+INSERT INTO shipment_methods(shipment_method, shipment_cost) values ('DHL', 12);
+INSERT INTO shipment_methods(shipment_method, shipment_cost) values ('Poczta', 8);
+
+INSERT INTO order_statuses(order_status) values ('INCOMPLETE');
+INSERT INTO order_statuses(order_status) values ('COMPLETE');
+INSERT INTO order_statuses(order_status) values ('BACKORDERED');
+INSERT INTO order_statuses(order_status) values ('CANCELED');
+INSERT INTO order_statuses(order_status) values ('DECLINED');
+
+INSERT INTO shipments(shipment_method_id, shipment_status_id) values ( 1, 1 );
+INSERT INTO payments(payment_method_id, payment_status_id) values ( 1, 1 );
