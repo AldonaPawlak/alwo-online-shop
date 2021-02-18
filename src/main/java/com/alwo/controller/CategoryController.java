bea5bb1a04e534ba7302b1d944c7e7cc023724a0
Aldona <aldonapawlak2@gamil.com>
2021-02-18 10:33:47 +0100

    Add new category and refactor code

--- src/main/java/com/alwo/controller/CategoryController.java
+++ src/main/java/com/alwo/controller/CategoryController.java
@@ -26,15 +26,12 @@ public class CategoryController {
     public List<Category> getCategories(@RequestParam(required = false) Integer page, Sort.Direction sort) {
         int pageNumber = page != null && page>= 0 ? page:0;
         Sort.Direction sortDirection = sort != null ? sort : Sort.Direction.ASC;
-//        return dtoMapper.mapToProductDtos(productService.getProducts(pageNumber, sortDirection));
         return categoryService.getCategories(pageNumber, sortDirection);
     }
 
     @GetMapping("/alwo/categories/{id}")
     @ResponseStatus(HttpStatus.OK)
-//    public ProductDto getProduct(@PathVariable long id){
     public Category getCategory(@PathVariable long id){
-//        return dtoMapper.mapToProductDto(productService.getProduct(id));
         return categoryService.getCategory(id);
     }
 
@@ -43,10 +40,4 @@ public class CategoryController {
     public Set<Category> getSelectedCategories(@RequestParam(required = false) List<String> categories) {
         return categoryService.getCategoriesBYNames(categories);
     }
-
-    // ONLY ADMIN
-//    @PutMapping("/admin/categories")
-//    public Category editCategory(@RequestBody Category category){
-//        return categoryService.editCategory(category);
-//    }
 }
