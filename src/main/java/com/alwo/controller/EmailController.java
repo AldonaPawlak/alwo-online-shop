52757e87ea8ceedbf0b011e6196411e4ee828ee6
Aldona <aldonapawlak2@gamil.com>
2021-02-19 12:28:58 +0100

    Refactor send email method

--- src/main/java/com/alwo/controller/EmailController.java
+++ src/main/java/com/alwo/controller/EmailController.java
@@ -2,7 +2,6 @@ package com.alwo.controller;
 
 import com.alwo.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -12,7 +11,12 @@ public class EmailController {
 
   @PostMapping("/alwo/contact")
   public void sendEmail(@RequestBody String email) {
-    System.out.println(email);
+    Context context = new Context();
+    context.setVariable("header", "New great books on Alwo BOOKSTORE!!! :-)");
+    context.setVariable("title", "Bestsellers!");
+    context.setVariable("description", "The Best Books!!!");
+    String body = templateEngine.process("template", context);
+    emailSender.sendEmail(email, "Alwo BOOKSTORE Newsletter", body);
   }
 
   private final EmailService emailSender;
@@ -25,14 +29,4 @@ public class EmailController {
     this.templateEngine = templateEngine;
   }
 
-  @GetMapping("/alwo/contact/email")
-  @ResponseStatus(HttpStatus.OK)
-  public void send(String email) {
-    Context context = new Context();
-    context.setVariable("header", "New greate books on Alwo BOOKSTORE!!! :-)");
-    context.setVariable("title", "Bestsellers!");
-    context.setVariable("description", "The Best Books!!!");
-    String body = templateEngine.process("template", context);
-    emailSender.sendEmail("aldonapawlak2@gmail.com", "Alwo BOOKSTORE Newsletter", body);
-  }
 }
